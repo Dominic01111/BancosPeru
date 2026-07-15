1ad5fe83e8d93da556981ad070cdcbc1c8a01dfe
Dominic01111 <dominicrack8@gmail.com>
2026-07-15 15:35:23 -0500

    Update ATM program with new PIN and balance
    modificacion para el banco - INTERBANK

--- CajeroAutomatico.py
+++ CajeroAutomatico.py
@@ -1,16 +1,16 @@
 # ===========================================
 # PROGRAMA: CAJERO AUTOMÁTICO
-# Autor: Dominic Novella
+# Autor: Dominic Novella 3ro de secundaria
 # ===========================================
 
-saldo = 5000
-pin_correcto = "5678"
+saldo = 20000
+pin_correcto = "9012"
 
 print("===================================")
-print("     BIENVENIDO AL CAJERO DEL BCP")
+print("     CAJERO INTERBANK")
 print("===================================")
 
-pin = input("Ingrese su clave PIN: ")
+pin = input("Ingresar PIN: ")
 
 if pin == pin_correcto:
 
